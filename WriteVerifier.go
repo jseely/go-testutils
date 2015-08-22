df7a9bbf9675d744b84ec955ee4133e2bbddd40b
Justin Seely <justin.seely@gmail.com>
2015-08-21 17:00:20 -0700

    Better documentation + FailWrite string

--- WriteVerifier.go
+++ WriteVerifier.go
@@ -3,13 +3,30 @@ package testutils
 import (
 	"bytes"
 	"errors"
-	"io"
 	"sync"
 	"testing"
 )
 
+const (
+	// FailWrite is the string that should be sent to a WriteVerfier to test
+	// Write failures. This string will cause Write calls to return -1,
+	// errors.New("I was told to fail this!")
+	FailWrite = "Fail this write!"
+)
+
+// WriteVerifier functions as an io.Writer allowing users to test code that should
+// write to an io.Writer interface. It calls it's testing.T instance's Error
+// on unexpected writes and then the user can call CheckDone to verify all expected
+// writes did indeed take place
 type WriteVerifier interface {
-	io.Writer
+	// Write emulates the Write method of io.Writers but instead compares it
+	// against an internal set of expected writes. If the write does not match an
+	// expected write that has yet to take place it will call testing.T.Error
+	Write(p []byte) (n int, err error)
+
+	// CheckDone should be called after all writes to the write should have
+	// completed. The object then validates that all expected writes were received,
+	// calling the Error method in the testing.T instance if any were missed.
 	CheckDone()
 }
 
@@ -21,6 +38,8 @@ type parallelWriteVerifier struct {
 	lock           *sync.RWMutex
 }
 
+// NewParallelWV creates a concurrency enabled WriteVerifier for testing code that
+// concurrently writes to io.Writers
 func NewParallelWV(name string, expectedWrites []string, t *testing.T) WriteVerifier {
 	return &parallelWriteVerifier{
 		name:           name,
@@ -31,6 +50,9 @@ func NewParallelWV(name string, expectedWrites []string, t *testing.T) WriteVeri
 	}
 }
 
+// CheckDone should be called after all writes to the write should have
+// completed. The object then validates that all expected writes were received,
+// calling the Error method in the testing.T instance if any were missed.
 func (v *parallelWriteVerifier) CheckDone() {
 	done := true
 	for _, m := range v.writeMatch {
@@ -43,6 +65,9 @@ func (v *parallelWriteVerifier) CheckDone() {
 	}
 }
 
+// Write emulates the Write method of io.Writers but instead compares it
+// against an internal set of expected writes. If the write does not match an
+// expected write that has yet to take place it will call testing.T.Error
 func (v *parallelWriteVerifier) Write(p []byte) (int, error) {
 	v.lock.Lock()
 	defer v.lock.Unlock()
@@ -51,7 +76,7 @@ func (v *parallelWriteVerifier) Write(p []byte) (int, error) {
 		if e == string(p) && v.writeMatch[i] == false {
 			v.writeMatch[i] = true
 
-			if string(p) == "Fail this write" {
+			if bytes.Equal(p, []byte(FailWrite)) {
 				return -1, errors.New("I was told to fail this!")
 			}
 			return len(p), nil
@@ -68,6 +93,8 @@ type sequentialWriteVerifier struct {
 	t              *testing.T
 }
 
+// NewSequentialWV creates a sequential WriteVerifier which can be used to test
+// code writing to an io.Writer interface non-concurrently
 func NewSequentialWV(name string, expectedWrites []string, t *testing.T) WriteVerifier {
 	return &sequentialWriteVerifier{
 		name:           name,
@@ -77,12 +104,18 @@ func NewSequentialWV(name string, expectedWrites []string, t *testing.T) WriteVe
 	}
 }
 
+// CheckDone should be called after all writes to the write should have
+// completed. The object then validates that all expected writes were received,
+// calling the Error method in the testing.T instance if any were missed.
 func (s *sequentialWriteVerifier) CheckDone() {
 	if s.currentIndex != len(s.expectedWrites) {
 		s.t.Errorf("%s writer did not receive all expected writes.", s.name)
 	}
 }
 
+// Write emulates the Write method of io.Writers but instead compares it
+// against an internal set of expected writes. If the write does not match an
+// expected write that has yet to take place it will call testing.T.Error
 func (s *sequentialWriteVerifier) Write(p []byte) (int, error) {
 	if s.currentIndex >= len(s.expectedWrites) {
 		s.t.Errorf("%s writer was not expecting a write but received \"%s\"", s.name, p)
@@ -92,7 +125,7 @@ func (s *sequentialWriteVerifier) Write(p []byte) (int, error) {
 		s.t.Errorf("%s writer at index %d expected \"%s\" but received \"%s\"", s.name, s.currentIndex, s.expectedWrites[s.currentIndex], p)
 	}
 	s.currentIndex++
-	if string(p) == "Fail this write" {
+	if bytes.Equal(p, []byte(FailWrite)) {
 		return -1, errors.New("I was told to fail this!")
 	}
 	return len(p), nil
